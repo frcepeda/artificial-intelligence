8c1e1e963903a760de47bc4df0d3127344887ce7
Freddy Román <me@freddy.mx>
2016-02-18 21:58:38 +0000

    Initial implementation of A* (Dijkstra.)

--- 2.n-puzzle/solver/src/Lib.hs
+++ 2.n-puzzle/solver/src/Lib.hs
@@ -1,22 +1,27 @@
 module Lib
     ( solvePuzzle
+    , stuff
     ) where
 
 import Data.Bits
 import Data.List
 import Data.Maybe
+import qualified Data.IntSet as S
+import qualified Data.PQueue.Min as P
 import Data.Word
 import Debug.Trace
 import Control.Applicative
 import Control.Monad
+import Control.Monad.Trans.State.Strict
 
-type Puzzle = Word64
+type Puzzle = Int -- Word64
 
-toPuzzle :: [Int] -> Word64
-toPuzzle = foldl' (\r x -> unsafeShiftL r 4 + fromIntegral x) 0
+pprint n p = show $ map (map (numAt n p)) [[(i,j) | j <- [0..n-1]] | i <- [0..n-1]]
+
+toPuzzle :: [Int] -> Puzzle
+toPuzzle = foldr (\x r -> unsafeShiftL r 4 + fromIntegral x) 0
 
 {-# INLINE numAt #-}
---numAt :: Integral a => a -> a -> (a,a) -> Puzzle
 numAt n p (i,j) = (unsafeShiftR p (4*(n*i + j))) .&. mask
     where mask = ((1 `unsafeShiftL` 4) - 1)
 
@@ -37,6 +42,27 @@ moves n p = catMaybes $ [swap] <*> deltas
 solvePuzzle :: Int -> [Int] -> Maybe [Int]
 solvePuzzle n xs = do
     guard $ sort xs == [0..n*n-1]
-    let first = head . moves n . snd
-    let movs = take 4 . tail $ iterate first (0,(toPuzzle xs))
-    return $ map fst movs
+    aStar n (toPuzzle xs) (toPuzzle [0..n*n-1])
+
+type AStar a = State (S.IntSet, P.MinQueue (Int, [Int], Puzzle)) a
+
+aStar :: Int -> Puzzle -> Puzzle -> Maybe [Int]
+aStar n start goal = evalState aStar' (S.empty, (P.singleton (0,[],start)))
+    where aStar' = do
+            (vis, queue) <- get
+            case P.minView queue of
+                Nothing -> return Nothing
+                (Just ((c,p,x), newQueue)) -> do
+                       let newVis = S.insert x vis
+                       put (newVis, newQueue)
+
+                       if x == goal
+                       then return $ Just (reverse p)
+                       else do forM_ (moves n x) $ \(m, next) -> do
+                                     unless (next `S.member` newVis) $ do 
+                                          modify (mapSnd $ P.insert (c+1, m:p, next))
+                               aStar'
+          aStar' :: AStar (Maybe [Int])
+
+mapSnd f (a,b) = (a,f b)
+stuff = solvePuzzle 2 [1,3,2,0]
