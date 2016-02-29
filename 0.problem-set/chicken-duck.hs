a048da40463b24c41db85f4c052583683691f1ce
Freddy Román <me@freddy.mx>
2016-02-29 05:08:34 +0000

    Added a solution to the Chicken and Duck problem.

---
+++ 0.problem-set/chicken-duck.hs
@@ -0,0 +1,18 @@
+import Control.Monad
+
+split (x:xs) = lefts ++ rights
+    where rest = split xs
+          lefts = map (\(a,b) -> (x:a,b)) rest
+          rights = map (\(a,b) -> (a,x:b)) rest
+split _ = [([],[])]
+
+pickOne [x] = [(x,[])]
+pickOne (x:xs) = (x,xs) : map (\(a,b) -> (a,x:b)) (pickOne xs)
+pickOne _ = []
+
+main = do
+    nums <- map read . words <$> getContents
+    forM_ (split nums) $ \(l,r) -> do
+        let sr = sum r
+        forM_ (pickOne l) $ \(x,l') -> do
+            when (sum l' == 2*sr) (print (x,(l', sum l'),(r, sum r)))
