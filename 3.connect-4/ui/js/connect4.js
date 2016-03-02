c896a568d8f62262987370bc6a596a3e56c3adcf
Freddy Román <me@freddy.mx>
2016-03-02 00:19:29 -0600

    Handle clicks in the canvas.

--- 3.connect-4/ui/js/connect4.js
+++ 3.connect-4/ui/js/connect4.js
@@ -2,12 +2,17 @@ function Connect4(r, c){
 	this.r = r;
 	this.c = c;
 
+	this.jcanvas = $('#game-window');
+
 	this.grid = [];
 	for (var i = 0; i < r; i++){
 		this.grid.push([]);
 		for (var j = 0; j < c; j++)
 			this.grid[i].push('');
 	}
+
+	$('#game-window').unbind('onclick');
+	$('#game-window').click(this.handleClick.bind(this));
 
 	this.state = 'READY';
 }
@@ -20,33 +25,47 @@ Connect4.prototype.begin = function(p1, p2){
 	this.state = 'PLAYING';
 }
 
-Connect4.prototype.step = function(){
+Connect4.prototype.step = function(callback){
 	if (this.state !== 'PLAYING')
 		throw 'connect4 step: invalid state';
 
-	var m = this.moveFunc[this.currentPlayer]();
-	this.play(m, this.currentPlayer);
-
-	this.currentPlayer = (this.currentPlayer + 1) % 2;
+	var self = this;
+	this.moveFunc[this.currentPlayer](function (m) {
+		var valid = self.play(m, self.currentPlayer);
+		if (valid)
+			self.currentPlayer = (self.currentPlayer + 1) % 2;
+		callback(valid);
+	});
 }
+
+Connect4.prototype.handleClick = function(e){
+	if (this.clickCallback){
+		var realX = e.clientX - this.jcanvas.offset().left;
+		var column = Math.floor(realX / this.block);
+		this.clickCallback(column);
+	}
+};
 
 Connect4.prototype.play = function(j, p){
 	var i = this.r;
 	for (; i > 0 && this.grid[i-1][j] === ''; i--);
-	if (i == this.r)
-		throw 'play: invalid move ' + j + ' by player ' + p;
+	if (i == this.r){
+		console.log('play: invalid move ' + j + ' by player ' + p);
+		return false;
+	}
 	this.grid[i][j] = p;
 	this.draw();
+	return true;
 }
 
 Connect4.prototype.draw = function(){
-	var c = $('#game-window');
+	var c = this.jcanvas;
 	var p = $(c).parent();
 
 	var pw = $(p).innerWidth();
 	var ph = $(p).innerHeight();
 
-	var block = Math.min(pw/this.c, ph/this.r);
+	var block = this.block = Math.min(pw/this.c, ph/this.r);
 	var r = .4*block;
 
 	var w = block*this.c;
@@ -96,11 +115,24 @@ $(document).ready(function (){
 
 	$(window).resize(connect4.draw.bind(connect4));
 
-	var randomPlayer = function(){
-		return Math.floor(Math.random() * 7);
+	var randomPlayer = function(callback){
+		window.setTimeout(function () {
+			callback(Math.floor(Math.random() * 7));
+		}, 700);
 	}
 
-	connect4.begin(randomPlayer, randomPlayer);
-	for (var i = 0; i < 10; i++)
-		connect4.step();
+	var waitingPlayer = function(callback){
+		connect4.jcanvas.css('cursor', 'pointer');
+		connect4.clickCallback = function (m){
+			connect4.jcanvas.css('cursor', 'auto');
+			connect4.clickCallback = null;
+			callback(m);
+		};
+	}
+
+	connect4.draw();
+	connect4.begin(waitingPlayer, randomPlayer);
+	var i = 0;
+	var loop = function () { connect4.step(loop); };
+	connect4.step(loop);
 });
