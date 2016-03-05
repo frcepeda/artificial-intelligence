b0b25dd3ee103a960a53514b270fbf94fcc2849f
Freddy Román <me@freddy.mx>
2016-03-05 02:54:22 +0000

    Added empty functions to connect4 solver.

--- 3.connect-4/solver/src/Connect4/Types.hs
+++ 3.connect-4/solver/src/Connect4/Types.hs
@@ -2,12 +2,22 @@
 
 module Connect4.Types
     ( Experience(..)
-    , BoardState
+    , BoardState(..)
     ) where
 
 import GHC.Generics
+import Data.Array.Unboxed
 
 data Experience = Novice
                 | Amateur
                 | Expert
     deriving (Show, Eq, Ord, Generic)
+
+data Player = A | B
+    deriving (Show, Eq)
+
+data BoardState = BoardState
+    { grid :: UArray (Int,Int) (Maybe Player) -- ^ The game grid
+    , top :: UArray Int Int -- ^ The next available row for every column
+    , player :: Player -- ^ whose turn it is
+    }
