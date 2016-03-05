aa332a8b9e626cbf505947cfee3824126f96b7a4
Freddy Román <me@freddy.mx>
2016-03-05 17:30:52 -0600

    Typo on Connect4.hs

--- 3.connect-4/solver/src/Connect4.hs
+++ 3.connect-4/solver/src/Connect4.hs
@@ -25,7 +25,7 @@ isFull b = undefined
 
 {-| The heuristic score for a state.
 It must satisfy the zero-sum property.
-(i.e. stateScore b = stateScore b' where b
+(i.e. stateScore b = - stateScore b' where b
 and b' have opposite player fields.)
 Also, a winning position must award inf/2
 points to the winning player.
